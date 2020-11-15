4135259d4bc6f9f31fcb90436322dd0c1ca599aa
Zeusere <74267168+Zeusere@users.noreply.github.com>
2020-11-15 16:45:36 +0100

    Update 06- redondeos y decimales.R

--- mis-scripts/Tema1/06- redondeos y decimales.R
+++ mis-scripts/Tema1/06- redondeos y decimales.R
@@ -4,12 +4,15 @@ print(sqrt(2),10)
 print(sqrt(2),3)
 
 floor(sqrt(2))
-#con print podemos decidir cuantos números queremos plasmar
-#con r debemos tener cuidado con el número de decimales que se van trasladando
+#con print podemos decidir cuantos n?meros queremos plasmar
+#con r debemos tener cuidado con el n?mero de decimales que se van trasladando
 print(pi,22)
 print(pi,20)
 round(sqrt(2),4)^2
 #la funcion round sirve para redondear
 
 floor(-3.45)
+
+2
+
 
