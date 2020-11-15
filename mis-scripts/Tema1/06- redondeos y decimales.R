49effb6e950a49b07e900cf38eabb9ca42dd482c
Zeusere <74267168+Zeusere@users.noreply.github.com>
2020-11-15 10:01:42 +0100

    10

---
+++ mis-scripts/Tema1/06- redondeos y decimales.R
@@ -0,0 +1,15 @@
+cos(60)
+cos(60*pi/180)
+print(sqrt(2),10)
+print(sqrt(2),3)
+
+floor(sqrt(2))
+#con print podemos decidir cuantos números queremos plasmar
+#con r debemos tener cuidado con el número de decimales que se van trasladando
+print(pi,22)
+print(pi,20)
+round(sqrt(2),4)^2
+#la funcion round sirve para redondear
+
+floor(-3.45)
+
