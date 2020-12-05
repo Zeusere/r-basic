2bb27301cb476739e4c55afdb50a0e86624b94f2
Zeusere <74267168+Zeusere@users.noreply.github.com>
2020-12-05 18:49:09 +0100

    1

--- mis-scripts/Tema1/08- Ejercicio funcion operaciones.R
+++ mis-scripts/Tema1/08- Ejercicio funcion operaciones.R
@@ -1,8 +1,34 @@
 opBasic=function(a,b){
+  print("Suma")
   print(a+b)
+  print("Resta")
   print(a-b)
+  print("Multiplicación")
   print(a*b)
+  print("Cociente división entera")
   print(a%/%b)
+  print("Resta división entera")
+  print(a%%b)
+  
+  
+}
+opBasic(6,4)
+
+#Si queremos que quede más claro
+
+opBasic=function(a,b){
+  print("Suma")
+  print(paste(sprintf("%i+%i =",a,b),a+b))
+  print(a+b)
+  print("Resta")
+  print(paste(sprintf("%i-%i =",a,b),a-b))
+  print(a-b)
+  print("Multiplicación")
+  print(paste(sprintf("%i*%i =",a,b),a*b))
+  print(a*b)
+  print("Cociente división entera")
+  print(a%/%b)
+  print("Resta división entera")
   print(a%%b)
   
   
---
+++ mis-scripts/Tema1/09- NUMEROS COMPLEJOS.R
@@ -0,0 +1,43 @@
+#NUMEROS COMPLEJOS
+a+bi
+# o también como complex(real=.., imaginary=...) o
+#complex(modulus=...., argument=...)
+
+# buscar en wikipedia numero complejo
+
+# Para operaciones
+sqrr
+
+z= 2+3i
+Re(z) #parte real ( la que queda sobre el eje de la x)
+
+
+3+2i
+class(3+2i)
+(3+2i)*5
+
+(3+2i)*(-1+3i)
+
+complex(real=pi,imaginary=sqrt(2))->z1
+z1
+
+#sqrt(-5)da error
+
+# la raiz cuadrada siempre devuelve la parte real positiva
+sqrt(as.complex(-5))
+# para obtener la otra parte hay que multiplicar por m-1
+
+
+sqrt(3+2i)
+sqrt(3+2i)*-1
+exp(3+2i)
+sin(3+2i)
+cos(3+2i)
+
+#Módulo = sqrt(Re(z)^2 + Im(z)^2)
+
+Mod(z1)
+
+Argumento= arctan(im(z)/Re(z))
+Arg(z1)
+# angulo de z1, recordar que los angulos en R van en radiales
